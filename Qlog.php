8af122b6f30b25230b31c874500ac9c386ccdb55
Lawrence Lagerlof <llagerlof@gmail.com>
2018-12-07 16:43:52 -0200

    Now the l() method returns the logged data

--- Qlog.php
+++ Qlog.php
@@ -2,10 +2,10 @@
 /**
  * Qlog
  *
- * PHP logger. Zero configuration. No dependencies. One static class. Human readable log.
+ * PHP logger for debugging purposes. Human readable log. Zero configuration. No dependencies. One static class.
  *
  * @package    Qlog
- * @version    1.0.2
+ * @version    2.0.0
  * @author     Lawrence Lagerlof <llagerlof@gmail.com>
  * @copyright  2018 Lawrence Lagerlof
  * @link       http://github.com/llagerlof/Qlog
@@ -21,27 +21,28 @@ class Qlog
      * @param mixed $variable The variable to be logged
      * @param string $variable_name A text to be the title of the variable in the log file
      *
-     * @return string|bool|null Return the full path of log file, or false on write error, or null if directory isn't writable
+     * @return string Return the logged data
      */
     public static function l($variable, $variable_name = null)
     {
+        $arr_variable = explode("\n", print_r($variable, true));
+        foreach($arr_variable as $i => $line) {
+            $arr_variable[$i] = '    ' . $line;
+        }
+        $output_variable = implode("\n", $arr_variable);
+        $separator = ((!is_array($variable) && !is_object($variable)) ? "\n" : '') . str_repeat('-', 80) . "\n\n";
+        $logged =
+            ((!empty($variable_name) && is_string($variable_name)) ? $variable_name . ":\n\n" : '') .
+            $output_variable . "\n" .
+            $separator;
+
         $backtrace = debug_backtrace();
         $log_location = $backtrace[0]['file'].'_qlog.log';
-        if (is_writable(dirname($log_location))) {
-            $arr_variable = explode("\n", print_r($variable, true));
-            foreach($arr_variable as $i => $line) {
-                $arr_variable[$i] = '    ' . $line;
-            }
-            $output_variable = implode("\n", $arr_variable);
-            $separator = ((!is_array($variable) && !is_object($variable)) ? "\n" : '') . str_repeat('-', 80) . "\n\n";
-            $logged =
-                ((!empty($variable_name) && is_string($variable_name)) ? $variable_name . ":\n\n" : '') .
-                $output_variable . "\n" .
-                $separator;
+        $write_success = is_writable(dirname($log_location)) ? file_put_contents($log_location, $logged, FILE_APPEND) : false;
 
-            return file_put_contents($log_location, $logged, FILE_APPEND) ? $log_location : false;
-        }
+        $open_html_pre = php_sapi_name() == 'cli' ? '' : '<pre>';
+        $close_html_pre = php_sapi_name() == 'cli' ? '' : '</pre>';
 
-        return null;
+        return $open_html_pre . ($write_success ? '' : '* ') . $logged . $close_html_pre;
     }
 }
